0d4ab721bb3c6d82b3ad2ddde95d4032129972a5
Timofey Rotin <162833896+Timomeg@users.noreply.github.com>
2025-02-15 18:09:20 +0000

    Task done

--- header.h
+++ header.h
@@ -5,10 +5,12 @@
 #endif //HEADER_H
 #include <string>
 #include <vector>
+#include <set>
 struct Student {
     std::string Name;
-    std::string LastName;
+    std::string GroupName;
     std::vector<unsigned> Ratings;
+    std::vector<std::string> Subjects;
 };
 class Group {
     std::vector<Student> list_of_students;
@@ -30,13 +32,38 @@ public:
         }
         return name;
     }
-    void sort() {
-        int size_vect{0};
+    // Задание 1
+    void SortByName() {
+        size_t size_vect{0};
 
         while (size_vect != list_of_students.size()) {
-            int min_vect = size_vect;
-            for (int i{size_vect}; i < list_of_students.size(); i++) {
+            size_t min_vect = size_vect;
+            for (size_t i{size_vect}; i < list_of_students.size(); i++) {
                 if (list_of_students[i].Name < list_of_students[min_vect].Name) {
+                    min_vect = i;
+                }
+            }
+            std::swap(list_of_students[min_vect], list_of_students[size_vect]);
+            size_vect ++;
+        }
+    }
+    // Задание 2
+private:
+    static int Arith_mean(const std::vector<unsigned>&vect) {
+        unsigned sum{};
+        for (size_t i{}; i < vect.size(); i++) {
+            sum += vect[i];
+        }
+        return sum / vect.size();
+    }
+public:
+    void SortByRating() {
+        size_t size_vect{0};
+
+        while (size_vect != list_of_students.size()) {
+            size_t min_vect = size_vect;
+            for (size_t i{size_vect}; i < list_of_students.size(); i++) {
+                if (Arith_mean(list_of_students[i].Ratings) < Arith_mean(list_of_students[min_vect].Ratings)) {
                     min_vect = i;
                 }
             }
@@ -44,5 +71,67 @@ public:
             std::swap(list_of_students[min_vect], list_of_students[size_vect]);
             size_vect ++;
         }
+    }
+    // Задание 3
+    size_t CountTwoness() {
+        size_t num{};
+        for (size_t i{}; i < list_of_students.size(); i++) {
+            for(size_t j{}; j < list_of_students.size(); j++) {
+                if (list_of_students[i].Ratings[j] == 2) {
+                    num += 1;
+                    break;
+                }
+            }
+        }
+        return num;
+    }
+    // Задание 4
+    size_t CountExcellent() {
+        size_t sum{};
+        for (size_t i{}; i < list_of_students.size(); i++) {
+            size_t num = 0;
+            for(size_t j{}; j < list_of_students[i].Ratings.size(); j++) {
+                if (list_of_students[i].Ratings[j] == 5) {
+                    num += 1;
+                }
+                if (num == 5) {
+                    sum += 1;
+                }
+            }
+        }
+        return sum;
+    }
+    // Задание 5
+    std::vector<Student> VectorMathExcellent() {
+        std::vector<Student> Math5;
+        size_t index{};
+
+        for (size_t i{}; i < list_of_students.size(); i++) {
+            for (size_t j{}; j < list_of_students[i].Subjects.size(); j++) {
+                if (list_of_students[i].Subjects[j] == "Math") {
+                    index = j;
+                    break;
+                }
+            }
+            if (index != 0) {
+                if (list_of_students[i].Ratings[index] == 5) {
+                    Math5.push_back(list_of_students[i]);
+                }
+            }
+        }
+        return Math5;
+    }
+    // Задание 6
+    std::vector<std::string> GroupsId() {
+        std::set<std::string> group_set{};
+        std::vector<std::string> group_list{};
+
+        for (size_t i{}; i < list_of_students.size(); i++) {
+            group_set.insert(list_of_students[i].GroupName);
+        }
+        for (const std::string& groupId : group_set) {
+            group_list.push_back(groupId);
+        }
+        return group_list;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include "header.h"
 #include <cassert>
 int main() {
-    Student student4 = {"Vasya", "iu9-54", {3, 5, 4, 5, 5, 4}};
-    Student student5 = {"Alice", "iu9-54", {5, 5, 4, 3, 5, 4}};
+    Student student4 = {
+        "Vasya",
+        "iu9-54",
+        {3, 5, 4, 5, 5, 4},
+        {"Math", "Physics", "IT", "English", "Ecology", "Literature"}
+    };
+    Student student5 = {
+        "Alice",
+        "iu9-54",
+        {5, 5, 4, 3, 5, 4},
+        {"Math", "Physics", "IT", "English", "Ecology", "Literature"}
+    };
 
     Group group;
     group.add(student4);
     group.add(student5);
-    group.sort();
+    group.SortByName();
+    group.SortByRating();
     assert(group.remove("Vasya") == student4.Name);
     assert(group.remove("Alice") == student5.Name);
     return 0;
